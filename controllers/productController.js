725064b983e7139e2d2defd2b09dafb7c205fccd
Connor Overton <50933561+connorjamesoverton@users.noreply.github.com>
2022-05-11 08:16:52 -0400

    Returns all the necessary information
    Does still need $numberDecimal removed

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,8 @@ const request = require('request');
 //Return product details on GET
 exports.product_detail_get = function(req, res, next) {
 
-    //This should NOT be a fixed URL, this should be an environment variable, but I've been busy and am running out of time
+    //These should be using async.parallel, not running serially
+    //This should NOT be a fixed URL, this should be an environment variable
     request('https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=' 
     + parseInt(req.params.id), function (product_detail_err, product_detail_results) {
         if (product_detail_err || product_detail_results.statusCode !== 200) { return next(product_detail_err)}
@@ -15,18 +16,20 @@ exports.product_detail_get = function(req, res, next) {
             .exec(function(product_price_err, product_price_results){
                 if (product_price_err) { return next(product_price_err); }
                 if (product_price_results==null) {//No Results
-                    var product_price_err = new Error('Product not found');
-                    err.status = 404;
-                    return next(product_priceerr);
+                    res.json(JSON.parse(product_detail_results.body).data.product.item.product_description.title);
                 }
-                //This is not quite right, it needs to be fixed, but I've got to go to bed
-                res.json(JSON.parse(product_detail_results.body).data.product.item.product_description.title + product_price_results[0]);
+                res.json({ "id" : product_price_results[0].id,
+                            "name" : JSON.parse(product_detail_results.body).data.product.item.product_description.title,
+                            "current_price" : 
+                                {"value" : product_price_results[0].current_price.value,
+                                "currency_code" : product_price_results[0].current_price.currency_code}
+                        });
             });
         }
       });
 }
 
-//Update product price on PUT
+//Update product price on PUT  
 exports.product_details_put = function(req, res, next) {
 
     // Assume the ID doesn't need validation / sanitization
